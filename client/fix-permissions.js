1763315eb8f41631c16cb2c8124bc5f4d2fc3130
rooke <rookec2335@gmail.com>
2025-11-20 09:30:19 -0500

    Update fix-permissions.js

--- client/fix-permissions.js
+++ client/fix-permissions.js
@@ -1,16 +1,1 @@
-// This ONLY runs the chmod command on Linux (Render).
-// It does nothing on Windows (local dev).
 
-const { execSync } = require("child_process");
-const os = require("os");
-
-if (os.platform() !== "win32") {
-  try {
-    execSync("chmod +x node_modules/.bin/react-scripts");
-    console.log("Permissions fixed for react-scripts");
-  } catch (err) {
-    console.log("Could not fix react-scripts permissions, but continuing...");
-  }
-} else {
-  console.log("Windows detected — skipping chmod.");
-}
